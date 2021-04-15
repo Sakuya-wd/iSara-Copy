491f5cb3d3a2be509db501a1b3a82a72238c29e5
Sakuya-wd <yamamoto1.527@gmail.com>
2021-04-15 12:38:18 +0900

    Dart Sassの自動コンパイルを作成しました

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
-gulp.task("default", function () {
-  return gulp.src("CSS/*.css")
+gulp.task("sass", function () {
+  return gulp.src("sass/*.scss")
+    .pipe(sass({outputStyle: "expanded"}))
     .pipe(postcss([
       autoprefixer({
         cascade: false
       })
     ]))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("css"));
 });
+
+gulp.task("watch", function () {
+  gulp.watch("sass/*.scss", ["sass"]);
+});
+
+gulp.task("default", ["sass"]);
