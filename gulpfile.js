b85362a2cbb1bb9811b1773da1a23a6f29e9abee
Sakuya-wd <yamamoto1.527@gmail.com>
2021-04-17 15:14:33 +0900

    Gulp3からGulp4の記法に変更、cssの圧縮及び拡張子変更を作成しました

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,41 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
-gulp.task("sass", function () {
+const sassCompile = () => {
   return gulp.src("sass/*.scss")
-    .pipe(sass({outputStyle: "expanded"}))
+    //scssをcssに変換を行う。
+    .pipe(sass({
+      outputStyle: "expanded"
+    }))
+
+    //ベンダープレフィックスの付与削除を行う。
+    //デフォルト設定のため、シェア率1%以上のブラウザ,
+    //最新の2バージョンのブラウザ, Firefox ESR版に対応する。
     .pipe(postcss([
       autoprefixer({
         cascade: false
       })
     ]))
+    
+    //cssの圧縮を行う。
+    .pipe(cleanCSS())
+
+    //圧縮ファイルの拡張子を.min.cssに変換を行う。
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+
     .pipe(gulp.dest("css"));
-});
+}
 
-gulp.task("watch", function () {
-  gulp.watch("sass/*.scss", ["sass"]);
-});
+const watch = (done) => {
+  //scssに変更がある時、自動でsassCompile関数の処理を行う。
+  gulp.watch("sass/*.scss", gulp.task("sassCompile"));
+  done();
+}
 
-gulp.task("default", ["sass"]);
+exports.default = sassCompile;
+exports.watch = watch;
